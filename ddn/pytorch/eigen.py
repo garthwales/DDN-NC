ec9b1359035e36f6674e982f5444e4cd6ccfccbd
Garth Wales <git@garth.wales>
2023-08-03 11:25:41 +1200

    useless file, copy for records

--- ddn/pytorch/eigen.py
+++ ddn/pytorch/eigen.py
@@ -39,3 +39,27 @@ class NC(AbstractDeclarativeNode):
     # x = torch.matmul(x, x.transpose(1, 2)) # positive definite :)
         # or
     # x = torch.matmul(u[:, :, 0:n], u[:, :, 0:n].transpose(1, 2)) # for n+1 rank matrix instead
+    
+    # make into deterministic form.. otherwise it doesn't matter if it works or not if can flip flop
+    # e.g. use 
+    # def uniform_solution_direction(self, u, u_ref=None):
+        # batch, m, n = u.shape
+        # direction_factor = 1.0
+
+        # if self.uniform_solution_method != 'skip':
+        #     if u_ref is None:
+        #         u_ref = u.new_ones(1, m, 1).detach()
+
+        #     direction = torch.einsum('bmk,bmn->bkn', u_ref, u)
+
+        #     if u_ref.shape[2] == n:
+        #         direction = torch.diagonal(direction, dim1=1, dim2=2).view(batch, 1, n)
+
+        #     if self.uniform_solution_method == 'positive':
+        #         direction_factor = (direction >= 0).float()
+        #     elif self.uniform_solution_method == 'negative':
+        #         direction_factor = (direction <= 0).float()
+
+        # u = u * (direction_factor - 0.5) * 2
+
+        # return u 
