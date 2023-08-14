56b955329430ba5a6e8adcf781500868b4f5f8e8
Garth Wales <git@garth.wales>
2023-08-14 23:16:57 +1200

    half completed exp code

--- apps/nc/exp.py
+++ apps/nc/exp.py
@@ -1,11 +1,17 @@
 from __future__ import print_function
 import argparse
+
 import torch
 import torch.nn.functional as F
 import torch.optim as optim
-from torchvision import datasets, transforms
+
+from torchvision import transforms
+from torch.utils.data import DataLoader, random_split
+from utils.utils import load_images_from_directory
+
 from torch.optim.lr_scheduler import StepLR
 
+from nets.nets import GenericNC, BasicCNN, BasicMLP
 
 def train(args, model, device, train_loader, optimizer, epoch):
     model.train()
@@ -42,14 +48,33 @@ def test(model, device, test_loader):
         test_loss, correct, len(test_loader.dataset),
         100. * correct / len(test_loader.dataset)))
 
+def get_dataloader(args):
+    # TODO implement
+    X_input = load_images_from_directory(args.dir_input, num=args.batch, size=args.size, gray=args.grayscale)
+    Q_true = load_images_from_directory(args.dir_output, num=args.batch, size=args.size, gray=True)
+    
+    validation_split = 0.2
+    dataset_size = len(dataset)
+    validation_size = int(validation_split * dataset_size)
+    train_size = dataset_size - validation_size
 
+    # Create random train and validation splits
+    train_dataset, validation_dataset = random_split(dataset, [train_size, validation_size])
+
+    # Create data loaders for train and validation sets
+    batch_size = 16
+    train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    validation_dataloader = DataLoader(validation_dataset, batch_size=batch_size, shuffle=False)
+    
 def main():
+    # TODO add more args to match in-out-experiment
+    
     # Training settings
-    parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
-    parser.add_argument('--batch-size', type=int, default=64, metavar='N',
+    parser = argparse.ArgumentParser(description='DDN-NC')
+    parser.add_argument('--batch-size', type=int, default=32, metavar='N',
                         help='input batch size for training (default: 64)')
-    parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
-                        help='input batch size for testing (default: 1000)')
+    parser.add_argument('--test-batch-size', type=int, default=100, metavar='N',
+                        help='input batch size for testing (default: 100)')
     parser.add_argument('--epochs', type=int, default=14, metavar='N',
                         help='number of epochs to train (default: 14)')
     parser.add_argument('--lr', type=float, default=1.0, metavar='LR',
@@ -58,11 +83,9 @@ def main():
                         help='Learning rate step gamma (default: 0.7)')
     parser.add_argument('--no-cuda', action='store_true', default=False,
                         help='disables CUDA training')
-    parser.add_argument('--no-mps', action='store_true', default=False,
-                        help='disables macOS GPU training')
     parser.add_argument('--dry-run', action='store_true', default=False,
                         help='quickly check a single pass')
-    parser.add_argument('--seed', type=int, default=1, metavar='S',
+    parser.add_argument('--seed', type=int, default=22, metavar='S',
                         help='random seed (default: 1)')
     parser.add_argument('--log-interval', type=int, default=10, metavar='N',
                         help='how many batches to wait before logging training status')
@@ -70,14 +93,11 @@ def main():
                         help='For Saving the current Model')
     args = parser.parse_args()
     use_cuda = not args.no_cuda and torch.cuda.is_available()
-    use_mps = not args.no_mps and torch.backends.mps.is_available()
 
     torch.manual_seed(args.seed)
 
     if use_cuda:
         device = torch.device("cuda")
-    elif use_mps:
-        device = torch.device("mps")
     else:
         device = torch.device("cpu")
 
@@ -94,14 +114,16 @@ def main():
         transforms.ToTensor(),
         transforms.Normalize((0.1307,), (0.3081,))
         ])
-    dataset1 = datasets.MNIST('../data', train=True, download=True,
-                       transform=transform)
-    dataset2 = datasets.MNIST('../data', train=False,
-                       transform=transform)
-    train_loader = torch.utils.data.DataLoader(dataset1,**train_kwargs)
-    test_loader = torch.utils.data.DataLoader(dataset2, **test_kwargs)
+    
+    # Create the ImageFolder dataset
+    dataloader = get_dataloader()
 
-    model = Net().to(device)
+    # model = Net().to(device) # TODO: fix
+    model = GenericNC(pre_net, args.size[0], args.net, args.mat_type, args.method)
+    model = model.to(device)
+    optimizer = torch.optim.AdamW(model.parameters(), lr=args.lr)
+    
+    # should probably just use AdamW but this might be fun later..
     optimizer = optim.Adadelta(model.parameters(), lr=args.lr)
 
     scheduler = StepLR(optimizer, step_size=1, gamma=args.gamma)
