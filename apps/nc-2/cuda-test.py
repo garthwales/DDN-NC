933ac5c52f7b099726d475845c8d3cc966cdf205
Garth Wales <git@garth.wales>
2023-08-24 21:24:25 +1200

    test memory allocation

--- apps/nc-2/cuda-test.py
+++ apps/nc-2/cuda-test.py
@@ -2,3 +2,21 @@ import os
 os.environ["CUDA_VISIBLE_DEVICES"] = "1" # just use one GPU on big machine
 import torch
 assert torch.cuda.device_count() == 1
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print('Using device:', device)
+print()
+
+#Additional Info when using cuda
+if device.type == 'cuda':
+    print(torch.cuda.get_device_name(0))
+    print('Memory Usage:')
+    print('Allocated:', round(torch.cuda.memory_allocated(0)/1024**3,1), 'GB')
+    print('Cached:   ', round(torch.cuda.memory_reserved(0)/1024**3,1), 'GB')
+    
+    A = torch.randn((96*96, 96*96), device=device)
+    
+    print(torch.cuda.get_device_name(0))
+    print('Memory Usage:')
+    print('Allocated:', round(torch.cuda.memory_allocated(0)/1024**3,1), 'GB')
+    print('Cached:   ', round(torch.cuda.memory_reserved(0)/1024**3,1), 'GB')
