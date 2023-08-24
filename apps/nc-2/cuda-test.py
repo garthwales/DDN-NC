d7997e2bdb64bd0ebd20428740deeecd5f2fd148
Garth Wales <git@garth.wales>
2023-08-24 21:31:17 +1200

    try specify within

--- apps/nc-2/cuda-test.py
+++ apps/nc-2/cuda-test.py
@@ -1,14 +1,15 @@
-import os
-os.environ["CUDA_VISIBLE_DEVICES"] = "1" # just use one GPU on big machine
+# import os
+# os.environ["CUDA_VISIBLE_DEVICES"] = "1" # just use one GPU on big machine
 import torch
-assert torch.cuda.device_count() == 1
+# assert torch.cuda.device_count() == 1
+print(torch.cuda.device_count())
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+device = torch.device('cuda:1' if torch.cuda.is_available() else 'cpu')
 print('Using device:', device)
 print()
 
 #Additional Info when using cuda
-if device.type == 'cuda':
+if device.type == 'cuda:1':
     print(torch.cuda.get_device_name(0))
     print('Memory Usage:')
     print('Allocated:', round(torch.cuda.memory_allocated(0)/1024**3,1), 'GB')
